416302a2c8985749da757afb4fbea9ca970277ed
paka-00 <azu27dai@gmail.com>
2021-12-06 18:53:24 +0900

    2021/12/06

---
+++ submissions/abc116/a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+template<class T> inline bool chM(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+using namespace std;
+using ll=long long;
+
+int main(){
+    int a,b,c;
+    cin>>a>>b>>c;
+    long ans=a*b/2;
+    cout<<ans<<endl;
+}
