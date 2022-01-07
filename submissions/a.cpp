bd40abd1d2d4187e245c756899b9e0adaa1bb5d0
paka-00 <azu27dai@gmail.com>
2022-01-07 15:21:04 +0900

    2022/01/07

---
+++ submissions/a.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+using namespace std;
+using ll=long long;
+
+int main(){
+    cout<<"654231"<<endl;
+}
