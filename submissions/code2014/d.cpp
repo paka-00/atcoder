fee83b1dc50900d3a9c4ea1129069f3edcf6fb67
paka-00 <azu27dai@gmail.com>
2022-04-07 17:56:45 +0900

    integer

---
+++ submissions/code2014/d.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#include <cassert>
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll  i = 0; i < (ll )(n); i++)
+const ll mod=1000000007;
+
+
+
+int main(){
+    ll n;cin>>n;
+    ll i,j;
+
+    cout<<n+1<<" "<<2<<endl;
+
+    
+}
+    
