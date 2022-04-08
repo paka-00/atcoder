f507ad4459adc170498893c7805e97eca4110a65
paka-00 <azu27dai@gmail.com>
2022-04-08 16:34:03 +0900

    abc085

---
+++ submissions/abc085/b.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+using namespace std;
+using ll=long long;
+
+int main(){
+    int n;cin>>n;
+    set<int>d;
+    for(int i=0;i<n;i++){int l;cin>>l;d.insert(l);}
+    cout<<d.size()<<endl;
+    }
+
