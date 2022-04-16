ab4f17534fd1bfb01ce8fc7e603e881418231233
paka-00 <azu27dai@gmail.com>
2022-04-16 20:42:09 +0900

    divnum

---
+++ submissions/abc190/d.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include <cassert>
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll  i = 0; i < (ll )(n); i++)
+const ll mod=1000000007;
+
+
+vector<long long> enum_divisors(long long N) {
+    vector<long long> res;
+    for (long long i = 1; i * i <= N; ++i) {
+        if (N % i == 0) {
+            res.push_back(i);
+            // 重複しないならば i の相方である N/i も push
+            if (N/i != i) res.push_back(N/i);
+        }
+    }
+    // 小さい順に並び替える
+    sort(res.begin(), res.end());
+    return res;
+}
+
+int main(){
+    ll n;cin>>n;
+    ll ans=0;
+    ll s2=2*n;
+    for(auto v:enum_divisors(s2)){
+        
+        if((v%2==0&&(s2/v)%2==1)||(v%2==1&&(s2/v)%2==0))ans++;
+    }
+    cout<<ans<<endl;
+}
+
