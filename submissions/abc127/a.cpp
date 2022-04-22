680cbe95b19a9cb42efa4c6c54fc0528b7f8ad5f
paka-00 <azu27dai@gmail.com>
2022-04-22 22:52:08 +0900

    abc127

---
+++ submissions/abc127/a.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#include <cassert>
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+
+using namespace std;
+using ll = long long;
+using vl=vector<ll>;
+using vvl=vector<vector<ll>>;
+#define rep(i, n) for (ll  i = 0; i < (ll )(n); i++)
+const ll mod=1000000007;
+
+int main(){
+    int a,b;cin>>a>>b;
+    if(a>=13)
+        cout<<b<<endl;
+    else if(a>5)
+        cout<<b/2<<endl;
+    else cout<<0<<endl;
+    return 0;
+    
+}
---
+++ submissions/abc127/b.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include <cassert>
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+
+using namespace std;
+using ll = long long;
+using vl=vector<ll>;
+using vvl=vector<vector<ll>>;
+#define rep(i, n) for (ll  i = 0; i < (ll )(n); i++)
+const ll mod=1000000007;
+
+int main(){
+    ll r,d,x;cin>>r>>d>>x;
+    for(int i=0;i<10;i++){
+        x=r*x-d;
+        cout<<x<<endl;
+    } 
+}
