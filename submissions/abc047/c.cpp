a582f6a549b2ae9e0f12b826d417933b9b343783
paka-00 <azu27dai@gmail.com>
2022-05-06 16:09:38 +0900

    reverse

---
+++ submissions/abc047/c.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include <cassert>
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+
+using namespace std;
+using ll = long long;
+using vl=vector<ll>;
+using vvl=vector<vector<ll>>;
+#define rep(i, n) for (ll  i = 0; i < (ll )(n); i++)
+const ll mod=1000000007;
+
+
+int main(){
+    string s;cin>>s;
+    char tmp=s[0];
+    ll  ans=0;
+    for(auto v:s){
+        if(v!=tmp)ans++;
+        tmp=v;
+    }
+    cout<<ans<<endl;
+}
