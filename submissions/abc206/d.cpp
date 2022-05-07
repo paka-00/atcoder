3ab4475a348b85afd1d7fb57659a4f1fa80c250b
paka-00 <azu27dai@gmail.com>
2022-05-07 14:37:40 +0900

    map_unionfind

---
+++ submissions/abc206/d.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+#include <cassert>
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+
+using namespace std;
+using ll = long long;
+using vl=vector<ll>;
+using vvl=vector<vector<ll>>;
+#define rep(i, n) for (ll  i = 0; i < (ll )(n); i++)
+const ll mod=1000000007;
+
+class UnionFind {
+public:
+    vector < ll > par; // 各元の親を表す配列
+    vector < ll > siz; // 素集合のサイズを表す配列(1 で初期化)
+
+    // Constructor
+    UnionFind(ll sz_): par(sz_), siz(sz_, 1) {
+        for (ll i = 0; i < sz_; ++i) par[i] = i; // 初期では親は自分自身
+    }
+    void init(ll sz_) {
+        par.resize(sz_);
+        siz.assign(sz_, 1);
+        for (ll i = 0; i < sz_; ++i) par[i] = i; // 初期では親は自分自身
+    }
+
+    // Member Function
+    // Find
+    ll root(ll x) { // 根の検索
+        while (par[x] != x) {
+            x = par[x] = par[par[x]]; // x の親の親を x の親とする
+        }
+        return x;
+    }
+
+    // Union(Unite, Merge)
+    bool merge(ll x, ll y) {
+        x = root(x);
+        y = root(y);
+        if (x == y) return false;
+        // merge technique（データ構造をマージするテク．小を大にくっつける）
+        if (siz[x] < siz[y]) swap(x, y);
+        siz[x] += siz[y];
+        par[y] = x;
+        return true;
+    }
+
+    bool issame(ll x, ll y) { // 連結判定
+        return root(x) == root(y);
+    }
+
+    ll size(ll x) { // 素集合のサイズ
+        return siz[root(x)];
+    }
+};
+
+int main(){
+    ll n;cin>>n;
+    vl a(n+10);
+    UnionFind u(200010);
+    ll ans=0;
+    rep(i,n)cin>>a[i+1];
+
+    for(int i=1;i<=n/2;i++){
+        if(a[i]==a[n+1-i])continue;
+        else{
+            u.merge(a[i],a[n+1-i]);
+        }
+    }
+    map<ll,bool>b;
+    for(int i=1;i<=n;i++){
+        if(!b[u.root(a[i])]){
+            ans+=u.size(u.root(a[i]))-1;
+            b[u.root(a[i])]=true;
+        }
+    }
+    cout<<ans<<endl;
+}
