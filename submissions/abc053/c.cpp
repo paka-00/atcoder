26e7ccdec4847058831d72b40aee6057804a1ab9
paka-00 <azu27dai@gmail.com>
2022-05-14 13:17:47 +0900

    dice

---
+++ submissions/abc053/c.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#include <cassert>
+#include <string>
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+
+using namespace std;
+using ll = long long;
+using vl=vector<ll>;
+using vvl=vector<vector<ll>>;
+#define rep(i, n) for (ll  i = 0; i < (ll )(n); i++)
+const ll mod=1000000007;
+
+int main(){
+    ll x;cin>>x;
+    ll t=x%11;
+    ll ans=(x/11)*2;
+    if(t>6)ans+=2;
+    else if(t>0)ans++;
+    cout<<ans<<endl;
+}
+
