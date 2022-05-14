215538577243f463142e0dea9e47eb062be88324
paka-00 <azu27dai@gmail.com>
2022-05-14 13:49:35 +0900

    integer_gcd_prime_f

---
+++ submissions/abc142/d.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include <cassert>
+#include <string>
+template<class T> inline bool chmax(T& a,T b){if(a<b){a=b;return true;}return false;}
+template<class T> inline bool chmin(T& a,T b){if(a>b){a=b;return true;}return false;}
+ 
+using namespace std;
+using ll = long long;
+using vl=vector<ll>;
+using vvl=vector<vector<ll>>;
+using ld=long double;
+using pll =pair<ll,ll>;
+#define rep(i,n)for(ll i=0;i<(ll)(n);i++)
+#define ssort(a)sort(a.begin(),a.end());
+
+vector<pair<long long, long long> > prime_factorize(long long N) {
+    vector<pair<long long, long long> > res;
+    for (long long a = 2; a * a <= N; ++a) {
+        if (N % a != 0) continue;
+        long long ex = 0;
+        while (N % a == 0) {
+            ++ex;
+            N /= a;
+        }
+        res.push_back({a, ex});
+    }
+    if (N != 1) res.push_back({N, 1});
+    return res;
+}
+
+int main(){
+    ll a,b;cin>>a>>b;
+    ll t=__gcd(a,b);
+    cout<<prime_factorize(t).size()+1<<endl;
+}
+
