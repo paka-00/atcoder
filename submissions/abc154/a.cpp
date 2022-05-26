455e69a624d62924fe976a5f07a047916257f17f
paka-00 <azu27dai@gmail.com>
2022-05-26 23:00:56 +0900

    basic

---
+++ submissions/abc154/a.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#include <cassert>
+
+template<class T> inline bool chmax(T& a,T b){if(a<b){a=b;return true;}return false;}
+template<class T> inline bool chmin(T& a,T b){if(a>b){a=b;return true;}return false;}
+ 
+using namespace std;
+using ll = long long;
+using vl=vector<ll>;
+using vvl=vector<vector<ll>>;
+using ld=long double;
+using pll =pair<ll,ll>;
+#define rep(i,n)for(ll i=0;i<(ll)(n);i++)
+#define ssort(a)sort(a.begin(),a.end());
+const ll mod=1000000007;
+
+
+int main(){
+    string s,t;cin>>s>>t;
+    int a,b;cin>>a>>b;
+    string u;cin>>u;
+    if(u==s)a--;
+    else b--;
+    cout<<a<<" "<<b<<endl;
+}
+
---
+++ submissions/abc154/c.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#include <cassert>
+
+template<class T> inline bool chmax(T& a,T b){if(a<b){a=b;return true;}return false;}
+template<class T> inline bool chmin(T& a,T b){if(a>b){a=b;return true;}return false;}
+ 
+using namespace std;
+using ll = long long;
+using vl=vector<ll>;
+using vvl=vector<vector<ll>>;
+using ld=long double;
+using pll =pair<ll,ll>;
+#define rep(i,n)for(ll i=0;i<(ll)(n);i++)
+#define ssort(a)sort(a.begin(),a.end());
+const ll mod=1000000007;
+
+
+int main(){
+    ll n;cin>>n;
+    set<ll> a;rep(i,n){
+        ll t;cin>>t;
+        a.insert(t);
+    }
+    if(n==a.size())cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+}
+
