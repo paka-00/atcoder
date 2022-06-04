a71d06bf4ab5fd9ddb792edc5c7708cd3427a2d1
paka-00 <azu27dai@gmail.com>
2022-06-04 16:30:09 +0900

    basic

---
+++ submissions/abc202/a.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#include <cassert>
+
+template<class T> inline bool chmax(T& a,T b){if(a<b){a=b;return true;}return false;}
+template<class T> inline bool chmin(T& a,T b){if(a>b){a=b;return true;}return false;}
+ 
+using namespace std;
+using ll = long long;
+using vl=vector<ll>;
+using vvl=vector<vector<ll>>;
+using ld=long double;
+using pll =pair<ll,ll>;
+#define rep(i,n)for(ll i=0;i<(ll)(n);i++)
+#define ssort(a)sort(a.begin(),a.end());
+#define gsort(a)sort(a.begin(),a.end(),greater<ll>());
+const ll mod=998244353;
+
+int main(){
+    ll a,b,c;cin>>a>>b>>c;
+    cout<<21-(a+b+c)<<endl;
+}
+
